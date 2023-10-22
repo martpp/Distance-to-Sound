4af0354b5c566054c02ef63da2ffa28a82815827
martin <martin@mpp.one>
2023-10-22 15:49:29 +0200

    3 sensors and filter

--- src/main.cpp
+++ src/main.cpp
@@ -17,29 +17,84 @@ Ultrasonic ultrasonic2(trigPin3, echoPin3);
 unsigned int duration_ms;
 unsigned int last_time_ms;
 
+class MovingAverageFilter
+{
+public:
+    MovingAverageFilter(int window_size) 
+        : windowsize(window_size), index(0), sum(0.0f) {
+        inputhistory = new float[windowsize];
+        for (int i = 0; i < windowsize; i++) {
+            inputhistory[i] = 0.0f;
+        }
+    }
+
+    ~MovingAverageFilter() {
+        delete[] inputhistory;
+    }
+
+    float update(float input) {
+        sum -= inputhistory[index];  // remove the oldest value from the sum
+        inputhistory[index] = input; // insert the new value
+        sum += input;                // add the new value to the sum
+        index = (index + 1) % windowsize; // move to the next position
+        return sum / windowsize;
+    }
+
+private:
+    float* inputhistory;
+    int windowsize;
+    int index;
+    float sum;
+};
+
+int windowsize = 4;
+MovingAverageFilter filter_0(windowsize);
+MovingAverageFilter filter_1(windowsize);
+MovingAverageFilter filter_2(windowsize);
+
+float fdist0, fdist1, fdist2;
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(ampPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
   unsigned int current_time_ms = millis();
-  unsigned int distance1 = ultrasonic1.read();
+  fdist0 = filter_0.update(ultrasonic0.read());
+  fdist1 = filter_1.update(ultrasonic1.read());
+  fdist2 = filter_2.update(ultrasonic2.read());
+  unsigned int distance = min(fdist0, min(fdist1, fdist2));
+
+  Serial.print(">dist0:");
+  Serial.println(ultrasonic0.read());
+  Serial.print(">dist0 filtered:");
+  Serial.println(fdist0);
+  Serial.print(">dist1:");
+  Serial.println(ultrasonic1.read());
+  Serial.print(">dist1 filtered:");
+  Serial.println(fdist1);
+  Serial.print(">dist2:");
+  Serial.println(ultrasonic2.read());
+  Serial.print(">dist2 filtered:");
+  Serial.println(fdist2);
+
   // map distance to a duration value between 200 and 2000ms
-  unsigned int duration1 = map(distance1, 10, 100, 50, 1000);
+  unsigned int duration1 = map(distance, 10, 100, 50, 1000);
 
 
-  if(current_time_ms - last_time_ms > duration1 && distance1 < 100 && distance1 > 10)
+  if(current_time_ms - last_time_ms > duration1 && distance < 100 && distance > 10)
   {
     last_time_ms = current_time_ms;
-    Serial.print("Distance1: ");
-    Serial.print(distance1);
-    Serial.print(" Duration1: ");
-    Serial.println(duration1);
     // play a tone with the duration
     tone(ampPin, 1000, duration1 / 2);
+    digitalWrite(LED_BUILTIN, HIGH);
+  }else{
+    digitalWrite(LED_BUILTIN, LOW);
   }
   delay(10);
 }
